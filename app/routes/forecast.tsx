0dbd9eba7d2cd007d69d4bc1feb855ca695659f7
Eric Sakmar <eric.sakmar@gmail.com>
2025-07-18 10:05:27 -0400

    initial forecast service

--- app/routes/forecast.tsx
+++ app/routes/forecast.tsx
@@ -1,8 +1,24 @@
+import { getSettings } from "~/services/settingsService";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { getForecast } from "~/services/forecastService";
+import { redirect } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Picnic Planner" }];
+}
+
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  var settings = getSettings();
+  if (settings === null) {
+    return redirect("/settings");
+  }
+
+  // fetch the forecast
+  const forecast = await getForecast(settings.latitude, settings.longitude);
+  console.log(forecast);
+
+  // fetch the history if requested
+  return { settings, forecast };
 }
 
 export default function Forecast() {
---
+++ app/services/forecastService.ts
@@ -0,0 +1,42 @@
+import { fetchWeatherApi } from "openmeteo";
+
+export async function getForecast(latitude: number, longitude: number) {
+  const params = {
+    latitude,
+    longitude,
+    daily: ["precipitation_probability_max", "temperature_2m_max"],
+    timezone: "America/New_York", // TODO get from client
+    forecast_days: 14,
+    wind_speed_unit: "mph",
+    temperature_unit: "fahrenheit",
+    precipitation_unit: "inch",
+  };
+
+  const url = "https://api.open-meteo.com/v1/forecast";
+  const responses = await fetchWeatherApi(url, params);
+
+  const response = responses[0];
+
+  const daily = response.daily()!;
+
+  const dates = [
+    ...Array(
+      (Number(daily.timeEnd()) - Number(daily.time())) / daily.interval()
+    ),
+  ]
+    .map(
+      (_, i) => new Date((Number(daily.time()) + i * daily.interval()) * 1000)
+    )
+    .map((d) => d.toLocaleDateString()); // 7/18/2025
+
+  const precipProbabilities = daily.variables(0)!.valuesArray()!;
+  const temperatureMaximums = daily.variables(1)!.valuesArray()!;
+
+  const forecast = dates.map((d, i) => ({
+    date: d,
+    precipProbability: precipProbabilities[i],
+    temperatureMax: temperatureMaximums[i],
+  }));
+
+  return forecast;
+}
